fabb183f2cd9bc27e246e8d73e82e95c51ee11a1
Riku Erkkilä <riku.erkkila@gmail.com>
2014-10-09 16:32:08 +0300

    Basic movement and simple colliders added

---
+++ darkmario/Assets/Scripts/ProtagonistMovement.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProtagonistMovement : MonoBehaviour 
+{
+	float maxSpeed = 400.0F;
+	float accelleration = 20.0F;
+	float jumpSpeed = 800.0f;
+
+
+	bool isOnGround = true;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		Vector2 velocity = rigidbody2D.velocity;
+
+		if (Input.GetKey (KeyCode.UpArrow) && isOnGround ) 
+		{
+			velocity.y = jumpSpeed;
+		}
+		
+
+		if (Input.GetKey (KeyCode.RightArrow))
+		{
+			velocity.x += accelleration;
+
+			if (velocity.x > maxSpeed)
+				velocity.x = maxSpeed;
+
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow))
+		{
+			velocity.x -= accelleration;
+			
+			if (velocity.x < -maxSpeed)
+				velocity.x = -maxSpeed;
+			
+		}
+
+		rigidbody2D.velocity = velocity;
+	}
+
+
+
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Ground") 
+		{
+			isOnGround = true;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Ground") 
+		{
+			isOnGround = false;
+		}
+	}
+
+
+}
