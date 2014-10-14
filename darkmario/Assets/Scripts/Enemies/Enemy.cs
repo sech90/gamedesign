f6ed326385a766cf31a83ad75425667a45a36fa7
Christian Cardin <ccardin.sech@gmail.com>
2014-10-14 05:12:45 +0300

    added few AI movement scripts, placefolder textures

---
+++ darkmario/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Enemies"),LayerMask.NameToLayer("Enemies"));
+		Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Enemies"),LayerMask.NameToLayer("Items"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ darkmario/Assets/Scripts/Motion/AISimpleMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AISimpleMovement : MonoBehaviour {
+
+	public enum DIRECTION {RIGHT, LEFT};
+	public DIRECTION direction = DIRECTION.RIGHT;
+	public float speed = 15.0f;
+	public LayerMask layermask;
+
+	private Transform frontCheck;
+	private Collider2D[] hits;
+
+
+	void Awake(){
+		hits = new Collider2D[1];
+		frontCheck = transform.Find("frontCheck").transform;
+
+		//We assume that the frontCheck is always on the right, in the first instance
+		if(direction == DIRECTION.LEFT)
+			Flip ();
+	}
+
+	void FixedUpdate ()
+	{
+		int collisions = Physics2D.OverlapPointNonAlloc(frontCheck.position,hits,layermask); 
+		if(collisions > 0){
+			Flip();
+		}
+			
+		
+		// Set the enemy's velocity to moveSpeed in the x direction.
+		rigidbody2D.velocity = new Vector2(transform.localScale.x * speed, rigidbody2D.velocity.y);	
+
+	}
+	
+	public void Flip()
+	{
+		// Multiply the x component of localScale by -1.
+		Vector3 currentScale = transform.localScale;
+		currentScale.x *= -1;
+		transform.localScale = currentScale;
+	}
+
+
+}
