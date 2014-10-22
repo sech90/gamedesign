b04a665aa98ab408440b9746e089b017a2e6ddff
Riku Erkkilä <riku.erkkila@gmail.com>
2014-10-23 01:24:42 +0300

    All the animations in, Mario changing facing

--- darkmario/Assets/Scripts/Motion/MarioMovement.cs
+++ darkmario/Assets/Scripts/Motion/MarioMovement.cs
@@ -6,6 +6,7 @@ public class MarioMovement : MonoBehaviour
 {
 	// pixels / s
 	float maxWalkSpeed 			= 468.75F;
+	float minWalkSpeed			= 50.0f;
 	float jumpSpeed 			= 1200.0f;
 
 	// pixels / s^2
@@ -14,15 +15,21 @@ public class MarioMovement : MonoBehaviour
 	float skiddingDecelleration = 1828.0F;
 
 
-	private SMBPhysicsBody body;
-	private ObstacleCollision obsColls;
+	SMBPhysicsBody body;
+	ObstacleCollision obsColls;
 
 	Animator anim;
-	bool  isJumping = false;
+	
+	private enum STATE { STANDING, RUNNING, JUMPING, SKIDDING };
+	private STATE state;
+
+	private enum DIRECTION {RIGHT, LEFT};
+	private DIRECTION direction = DIRECTION.RIGHT;
 
 
 	void Start () 
 	{
+		state = STATE.STANDING;
 		anim = GetComponent<Animator> (); 
 		
 	}
@@ -32,35 +39,63 @@ public class MarioMovement : MonoBehaviour
 		body = gameObject.GetComponent<SMBPhysicsBody>();
 		obsColls = gameObject.GetComponent<ObstacleCollision>();
 	}
-	
-	
+
+	void SetState( STATE newState )
+	{
+		if (state == newState)
+			return;
+
+		state = newState;
+
+		switch (state) 
+		{
+				case STATE.STANDING: 
+						anim.SetTrigger ("Standing");
+						break;
+				case STATE.RUNNING: 
+						anim.SetTrigger ("Running");
+						break;
+				case STATE.JUMPING: 
+						anim.SetTrigger ("Jumping");
+						break;
+				case STATE.SKIDDING: 
+						anim.SetTrigger ("Skidding");
+						break;
+		}
+	}
+
+
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		// If Jump-button is just pressed down and Mario is standing on ground
-		if (UserInput.JumpDown() && obsColls.IsGrounded() ) 
+		if (UserInput.JumpDown() && IsGrounded() ) 
 		{
 			body.velocity.y = jumpSpeed;
-			anim.SetTrigger ("Jump");
-			isJumping = true;
+			SetState(STATE.JUMPING);
 		}
 
-		if (isJumping && body.velocity.y <= 0.0f && obsColls.IsGrounded() )
+		if ( IsGrounded() )
 		{
-			isJumping = false;
-			anim.SetTrigger("Run");
+			// If Mario is moving other way and player steeres other way, Mario skids and decellerates faster
+			if ( UserInput.Right() && body.velocity.x < 0.0f ||
+			     UserInput.Left()  && body.velocity.x > 0.0f    )
+			{
+				Decellerate( skiddingDecelleration * Time.deltaTime );
+				SetState(STATE.SKIDDING);
+			}
+			else if (Mathf.Abs (body.velocity.x) < minWalkSpeed)
+			{
+				SetState(STATE.STANDING);
+			}
+			else
+			{
+				SetState(STATE.RUNNING);
+			}
 		}
 
-		// If jump-button is being held down, gravity is lower making jump higher
-		if (UserInput.Jump() ) 
-		{
-			body.marioGravity = 2250.0f;
-		}
-		else
-		{
-			body.marioGravity = 7875.0f;
-		}
+
 		
 
 
@@ -89,27 +124,40 @@ public class MarioMovement : MonoBehaviour
 		}
 
 
-		// If Mario is moving other way and plahyer steeres other way, Mario skids and decellerates faster
-		if ( UserInput.Right() && body.velocity.x < 0.0f )
+		// Mario is facing the direction it's moving
+		if (body.velocity.x > 1.0f)
 		{
-			body.velocity.x += skiddingDecelleration * Time.deltaTime;
+			direction = DIRECTION.RIGHT;
+			// The sprite is flipped using x-scale of tranformation
+			Vector3 scale = transform.localScale;
+			scale.x = 1;
+			transform.localScale = scale;
 		}
-		
-		if ( UserInput.Left() && body.velocity.x > 0.0f)
+
+		if (body.velocity.x < -1.0f)
 		{
-			body.velocity.x -= skiddingDecelleration * Time.deltaTime;
+			direction = DIRECTION.LEFT;
+			Vector3 scale = transform.localScale;
+			scale.x = -1;
+			transform.localScale = scale;
 		}
 
 
-	
+		// If jump-button is being held down, gravity is lower making jump higher
+		if (UserInput.Jump() ) 
+		{
+			body.marioGravity = 2250.0f;
+		}
+		else
+		{
+			body.marioGravity = 7875.0f;
+		}
+
 
 	}
 
 
-
-
-
-	void Decellerate( float deltaV )
+	private void Decellerate( float deltaV )
 	{
 		if ( Mathf.Abs(body.velocity.x) < deltaV )
 		{
@@ -127,7 +175,13 @@ public class MarioMovement : MonoBehaviour
 			}	
 		}
 	}
-	
+
+	// Mario is grounded if he's touching ground and not moving upwards
+	private bool IsGrounded()
+	{
+		return (body.velocity.y < jumpSpeed / 2.0F && obsColls.IsGrounded ());
+	}
+
 	
 	
 	
