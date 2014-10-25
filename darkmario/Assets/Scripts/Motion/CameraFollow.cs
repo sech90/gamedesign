1e2a37db17303c0a3b66c83a360a110083c77997
Riku Erkkilä <riku.erkkila@gmail.com>
2014-10-25 18:11:42 +0300

    Foreground test, ground graphics, better camera

--- darkmario/Assets/Scripts/Motion/CameraFollow.cs
+++ darkmario/Assets/Scripts/Motion/CameraFollow.cs
@@ -5,6 +5,7 @@ public class CameraFollow : MonoBehaviour
 {
 
 	public Transform target;
+	public float xDistanceTarget = -150.0f;
 
 	void Start(){
 		if(target == null)
@@ -12,6 +13,13 @@ public class CameraFollow : MonoBehaviour
 	}
 	void Update () 
 	{
-		transform.position = new Vector3 (target.position.x, transform.position.y, transform.position.z);
+		float xDistance = target.position.x - transform.position.x;
+
+		if (xDistance > xDistanceTarget) 
+		{
+			transform.position = new Vector3 (target.position.x - xDistanceTarget, 
+			                                  transform.position.y, transform.position.z);
+		}
+
 	}
 }
