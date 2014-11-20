336e4e3e53de17e9687b68d5f4bd61e9bec0a31b
Riku Erkkilä <riku.erkkila@gmail.com>
2014-11-20 17:00:10 +0200

    Revert 068d5ec..7a70736
    This rolls back to commit 068d5ec9cfa467732a26d268be66a452227c2d52.

---
+++ shipshooter/Assets/Scripts/SeaHandler.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeaHandler : EditableMesh {
+
+
+
+	void Start () 
+	{
+		Create();
+	}
+	
+	void Update () 
+	{
+		
+		int i = 0;
+		
+		float time = Time.time;
+		
+		while (i < 81) 
+		{
+			Vector3 position = GetVertex(i,1); 
+			Vector3 newPosition = new Vector3( position.x, 
+			                                  GetSurfaceY( position.x, time ), 
+			                                  position.z );
+			
+			SetVertex(i,1, newPosition);
+			i++;
+		}
+
+		UpdateVertices();
+		
+	}
+
+
+	public float GetSurfaceY( float x, float time)
+	{
+//		return 4.0f + 0.8f * Mathf.Sin(time*0.4f + x*0.7f) 
+//			- 0.1f * Mathf.Cos(time*2 + x*2.0f);
+		return 4.0f + 0.3f * Mathf.Sin(time*0.4f + x*0.7f) 
+			- 0.1f * Mathf.Cos(time*2 + x*2.0f);
+		/*return 4.0f + 0.3f * Mathf.Sin(time*0.4f + x*0.7f) 
+			- 0.1f * Mathf.Cos(time*0.13f + x*2.0f); */
+	}
+
+}
---
+++ shipshooter/Assets/Scripts/ShipHandler.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipHandler : MonoBehaviour {
+
+	public GameObject sea;
+
+	public float hullWidth = 3.0f;
+	public float rollFactor = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		/*
+		SeaHandler sh = sea.GetComponent<SeaHandler>();
+
+
+		// Height of the sea 
+		float seaLeft   = sh.GetSurfaceY(7.5f - hullWidth/2.0f, Time.time);
+		float seaMiddle = sh.GetSurfaceY(7.5f,                  Time.time);
+		float seaRight  = sh.GetSurfaceY(7.5f + hullWidth/2.0f, Time.time);
+
+		// Ship floats at the avg of sea's surface at the three points
+		float shipHeight = ( seaLeft + seaMiddle + seaRight ) / 3.0f;
+
+
+
+		transform.position = new Vector3(transform.position.x, 
+		                                 shipHeight, 
+		                                 transform.position.z );
+
+
+		transform.rotation =  Quaternion.AngleAxis(Mathf.Sin(Time.time) * 5.0f, Vector3.forward);
+
+	*/
+		UpdateTransformation();
+	}
+
+
+	// Updates ships position and rotation so it floats on the waves
+	void UpdateTransformation()
+	{
+
+		SeaHandler sh = sea.GetComponent<SeaHandler>();
+			
+			
+		// Height of the sea at hull's middle and it's edges
+		float seaLeft   = sh.GetSurfaceY(7.5f - hullWidth/2.0f, Time.time);
+		float seaMiddle = sh.GetSurfaceY(7.5f,                  Time.time);
+		float seaRight  = sh.GetSurfaceY(7.5f + hullWidth/2.0f, Time.time);
+			
+		// Ship floats at the avg of the three points
+		float shipHeight = ( seaLeft + seaMiddle + seaRight ) / 3.0f;
+			
+			
+			
+		transform.position = new Vector3(transform.position.x, 
+			                             shipHeight, 
+			                             transform.position.z );
+
+		// Calculate ships roll (rotation caused by waves)
+		float roll = Mathf.Atan( (seaRight-seaLeft)/hullWidth ) * Mathf.Rad2Deg;
+		roll = roll * rollFactor; // dampen the roll to make it more tolerable
+			
+		transform.rotation =  Quaternion.AngleAxis(roll, Vector3.forward);
+	}
+
+}
