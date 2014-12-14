7873bcf45b578d221057beb5f1dfdb1e2cc25389
Christian Cardin <ccardin.sech@gmail.com>
2014-12-15 00:24:56 +0200

    start screen, small fixes

--- shipshooter/Assets/Scripts/GameHandler.cs
+++ shipshooter/Assets/Scripts/GameHandler.cs
@@ -14,8 +14,6 @@ public class GameHandler : MonoBehaviour {
 
 	public int ScorePerSecond = 50;
 	public Text ScoreText;
-	public AudioClip MainSoundtrack;
-	public AudioClip StartScreenAudio;
 
 	public float Score{get{return _score;}}
 
@@ -32,26 +30,6 @@ public class GameHandler : MonoBehaviour {
 		Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Markers"), LayerMask.NameToLayer("InteractiveObj"), true); 
 
 		flyingLionPrefab = Resources.Load<GameObject>("FlyingLion");
-//		GameObject flyingLion1 = Instantiate (flyingLionPrefab) as GameObject;
-//		GameObject flyingLion2 = Instantiate (flyingLionPrefab) as GameObject;
-//		GameObject flyingLion3 = Instantiate (flyingLionPrefab) as GameObject;
-		//flyingLion.transform.position = new Vector3(9.0f, 2.0f, 0.0f);
-
-		/** /
-		GameObject cameraPrefab = Resources.Load<GameObject>("Main Camera");
-		GameObject shipPrefab = Resources.Load<GameObject>("Ship");
-		GameObject sailorPrefab = Resources.Load<GameObject>("Sailor");
-
-		GameObject ship = Instantiate (shipPrefab) as GameObject;
-		GameObject sailor = Instantiate (sailorPrefab) as GameObject;
-		GameObject camera = Instantiate (cameraPrefab) as GameObject;
-		camera.GetComponent<CameraFollow2d>().targetGameObject = ship;
-
-		ship.transform.position = new Vector3(7.0f, 0.0f, 0.0f);
-		sailor.transform.parent = ship.transform;
-		sailor.transform.localPosition = new Vector3(0.0f, 0.0f, -1.0f);
-		/**/
-		//AudioSource.PlayClipAtPoint(MainSoundtrack,transform.position);
 	}
 
 	public static void AddScore(int amount){
---
+++ shipshooter/Assets/Scripts/General/StartScreen.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartScreen : MonoBehaviour {
+
+	public Button ShowHowTo;
+	public Button CloseHowTo;
+	public AudioClip TransitionSound;
+	public Text FadeText;
+
+	private GameObject _panel;
+
+
+	void Start(){
+		_panel = GameObject.FindGameObjectWithTag("HowTo");
+		ShowHowTo.onClick.AddListener(showInstructionPanel);
+		CloseHowTo.onClick.AddListener(hideInstructionPanel);
+		_panel.SetActive(false);
+	}
+
+	void Update(){
+
+	 
+
+		if(Input.anyKeyDown)
+			if(!Input.GetKeyDown(KeyCode.Mouse0) && !_panel.activeInHierarchy)
+				Application.LoadLevel("MainScene");
+		
+	}
+
+
+	private void showInstructionPanel(){
+		AudioSource.PlayClipAtPoint(TransitionSound,Vector3.zero);
+		_panel.SetActive(true);
+	}
+
+	private void hideInstructionPanel(){
+		AudioSource.PlayClipAtPoint(TransitionSound,Vector3.zero);
+		_panel.SetActive(false);
+	}
+}
